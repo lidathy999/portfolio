654e179bb926519cf497a5fde7c28341dcd42b75
@Thylida <lidathy.999@gmail.com>
2025-07-23 21:53:23 +0700

    init project

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
+
+function LoadingScreen({ onFinish }: { onFinish: () => void }) {
+  const [showTitle, setShowTitle] = useState(true);
+
+  useEffect(() => {
+    const timer1 = setTimeout(() => setShowTitle(false), 1800); // Show name for 1.8s
+    const timer2 = setTimeout(onFinish, 3500); // Show "Front-end Developer" for 1.7s
+    return () => {
+      clearTimeout(timer1);
+      clearTimeout(timer2);
+    };
+  }, [onFinish]);
+
+  return (
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-amber-50 z-50">
+      {showTitle ? (
+        <h1 className="text-5xl font-bold animate-fade-in">Lida</h1>
+      ) : (
+        <h2 className="text-2xl font-semibold animate-typewriter">
+          Front-end Developer
+        </h2>
+      )}
+    </div>
+  );
+}
+
+export default LoadingScreen;
---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+const MENUS = [
+  { label: "Home", id: "home" },
+  // { label: "About", id: "aboutMe" },
+  { label: "Resume", id: "resume" },
+  { label: "Projects", id: "projects" },
+  { label: "Contact", id: "contactMe" },
+];
+
+export { MENUS };
