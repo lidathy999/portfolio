a2886354b3467bb6eee9b1fec26f04ebfd7f3811
@Thylida <lidathy.999@gmail.com>
2025-11-03 21:15:21 +0700

    done

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
    // output: "export",
    // reactStrictMode: true,
+   transpilePackages: ['@mui/material', '@mui/system', '@mui/icons-material'],
 };
 
 export default nextConfig;
